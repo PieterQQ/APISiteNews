fa14fc09163cbccf9c7f07731c36a556bda2a309
PieterQQ <pieter444@wp.pl>
2020-03-25 17:21:01 +0100

    api connection

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using Newtonsoft.Json;
+using WebApplication4.Models;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+
+            WebClient wc = new WebClient();
+            
+                var url = "http://newsapi.org/v2/top-headlines?" +
+                                "country=gb&" +
+                                "apiKey=df3685a393f046e3aa729dea77412939";
+                var NewsString = wc.DownloadString(url);
+            byte[] bytes = Encoding.Default.GetBytes(NewsString);
+            NewsString = Encoding.UTF8.GetString(bytes);
+            RootObject datalist = JsonConvert.DeserializeObject<RootObject>(NewsString);
+           return View(datalist);
+        }
+    
+    }
+}
