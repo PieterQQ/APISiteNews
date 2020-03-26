b1a81c3dc63b8507c4e0522e87eadca8dc44e9dd
PieterQQ <pieter444@wp.pl>
2020-03-26 10:06:59 +0100

    Upgreade reiqured :(

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -14,15 +14,13 @@ namespace WebApplication4.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(string date)
         {
-
+           var text= Convert.ToDateTime(date).ToString("yyyy-MM-dd");
 
             WebClient wc = new WebClient();
-            
-                var url = "http://newsapi.org/v2/top-headlines?" +
-                                "country=gb&" +
-                                "apiKey=df3685a393f046e3aa729dea77412939";
+            var url = "http://newsapi.org/v2/everything?" +
+                                "uk&from=" + text+   "&to="+text+ "&sortBy=popularity&apiKey=df3685a393f046e3aa729dea77412939";
                 var NewsString = wc.DownloadString(url);
             byte[] bytes = Encoding.Default.GetBytes(NewsString);
             NewsString = Encoding.UTF8.GetString(bytes);
